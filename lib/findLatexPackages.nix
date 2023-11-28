c60d3984eeab451f1069ad044ce2a2b922a18755
jackyliu16 <18922251299@163.com>
2023-11-28 08:44:44 +0000

    update

--- lib/findLatexPackages.nix
+++ lib/findLatexPackages.nix
@@ -5,22 +5,30 @@
   pkgs,
   lib,
   ...
-}: let
+}: 
+let
+  inherit (builtins) trace match head tail split filter readFile isList isNull elemAt pathExists;
+  inherit (lib) concatMap concatLists subtractLists splitString genAttrs unique remove
+    hasSuffix isDerivation;
 in
   {fileContents}:
     with pkgs.lib.attrsets;
     with pkgs.lib.strings; 
     let 
       contentsLines = builtins.splitString "\n" fileContents;
-      preprocessLines = builtins.filter (line: !(isPackageLines line)) contentsLines;
-      processedLines = builtins.map ifPackageNameFormatCorrect preprocessLines;
-      texPackages = filterAttrs (y: x: x != null) (genAttrs packageNames (name: attrByPath [name] null pkgs.texlive));
+      # Check if line contains package information
+      preprocessLines = builtins.filter (line: !(isPackageLines line)) contentsLines; # List[str]: the line contains package info
+      processedPackages = unique (builtins.concatMap gainPackageNameFromLine preprocessLines); # List[str]: the line contains package name
+
+      texPackages = filterAttrs (y: x: x != null) processedPackages (genAttrs  (name: attrByPath [name] null pkgs.texlive));
 
       isPackageLines = line: let # str -> Bool
         res = builtins.match ''\\(usepackage|Requirepackage).*'' line;
       in res != null;
-      gainPackageName = line: let # str -> List[str]
+      gainPackageNameFromLine = line: let # str -> List[str]
         matchers = builtins.tail (builtins.match ''\\(usepackage|RequirePackage).*\{(.*)}}'' line); # [ ~~"usepackage"~~ "a" "b" ]
+        # Check if each name provide by matcher is correct
+        # TODO: add more trace output when incorrect.
         res = builtins.filter (ps: ps != false) builtins.map ifPackageNameFormatCorrect matchers;   
       in res;
       ifPackageNameFormatCorrect = one: let # str -> Bool
