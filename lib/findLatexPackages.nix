5c7fdb8e09d8171a7c4f889ab3a14f3c06bd3be8
jackyliu16 <18922251299@163.com>
2023-11-28 08:16:04 +0000

    fix

--- lib/findLatexPackages.nix
+++ lib/findLatexPackages.nix
@@ -9,49 +9,22 @@
 in
   {fileContents}:
     with pkgs.lib.attrsets;
-    with pkgs.lib.strings; let
-      buildCTANRegex = n: let
-        prefix = ''^\\usepackage.*\{(.*)\}.*% CTAN:'';
-        packageName = ''(.*)'';
-        suffix = ''.*$'';
-
-        reps = pkgs.lib.lists.replicate n packageName;
-        str = pkgs.lib.strings.concatStringsSep " " reps;
-      in
-        prefix + str + suffix;
-
-      processLine = line: n: let
-        regex = buildCTANRegex n;
-        matches = builtins.match regex line;
-        next =
-          if (matches != null)
-          then (processLine line (n + 1))
-          else null;
-      in
-        if (next != null)
-        then next
-        else matches;
-
-      lineToPackageNames = (
-        line: let
-          exact = builtins.match ''\\(usepackage|RequirePackage).*\{(.*)}'' line;
-          multicomment = processLine line 1;
-        in
-          (
-            if exact == null
-            then []
-            else exact
-          )
-          ++ (
-            if multicomment == null
-            then []
-            else multicomment
-          )
-      );
-
-      lines = splitString "\n" fileContents;
-      processedLines = builtins.filter (x: x != null) (builtins.map lineToPackageNames lines);
-      packageNames = builtins.concatLists processedLines;
+    with pkgs.lib.strings; 
+    let 
+      contentsLines = builtins.splitString "\n" fileContents;
+      preprocessLines = builtins.filter (line: !(isPackageLines line)) contentsLines;
+      processedLines = builtins.map ifPackageNameFormatCorrect preprocessLines;
       texPackages = filterAttrs (y: x: x != null) (genAttrs packageNames (name: attrByPath [name] null pkgs.texlive));
+
+      isPackageLines = line: let # str -> Bool
+        res = builtins.match ''\\(usepackage|Requirepackage).*'' line;
+      in res != null;
+      gainPackageName = line: let # str -> List[str]
+        matchers = builtins.tail (builtins.match ''\\(usepackage|RequirePackage).*\{(.*)}}'' line); # [ ~~"usepackage"~~ "a" "b" ]
+        res = builtins.filter (ps: ps != false) builtins.map ifPackageNameFormatCorrect matchers;   
+      in res;
+      ifPackageNameFormatCorrect = one: let # str -> Bool
+        res = builtins.match ''^[a-z](?|.*--)[a-z-]*[a-z]$'' one;
+      in res != null;
     in
-      texPackages
+     gainPackageName 
